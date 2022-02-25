3136b6ca4b3b8e610fe5fafe1d5834f3db932b26
Lalit Agrawal <lalitagrawal28june2001@gmail.com>
2022-02-25 19:51:13 +0530

    removed redundant files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import faker from '@faker-js/faker';
 
+
+const appCss={
+   textAllign:'center',
+   justifyContent:'center',
+   color:'silver',
+   border:'3px solid black'
+}
 function App() {
+  function getTime() {
+    return (new Date()).toLocaleTimeString()
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={appCss}>
+      <h1 >Cool {getTime()} </h1>
+      <i className="massive snowflake icon" style={{color:'grey'}}/>
+      <i className="massive sun icon" style={{color:'orange'}}/>
+      <h2> Hi {faker.animal.type()}</h2>
     </div>
   );
 }
