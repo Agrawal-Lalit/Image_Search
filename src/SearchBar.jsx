3258fc5df0cb65b7059ca21d16a06057bcf08a4c
Lalit Agrawal <lalitagrawal28june2001@gmail.com>
2022-03-02 11:44:33 +0530

    access key replaced before remote connection

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import axios from 'axios';
 import './App.css';
 
-const id='X90pt6oipcVMjX8-ZKI92yJpq3ZiHc7Xz3ZuX9P3aG4';
+const id='999999999999999999000000000';//replace access key from unsplash
 class SearchBar extends React.Component{
     
     state={searchItem: '',imageUrl:[]}
     handlerForChange = async (e)=>{
     
         this.setState({ searchItem: e.target.value});
+		//here in below console log the state change will be shown after rerendering so it will show old data//
         console.log(this.state.searchItem);
         const response= await axios.get(
          'https://api.unsplash.com/search/photos',{
@@ -22,7 +23,7 @@ class SearchBar extends React.Component{
         return(
         <div>
             <h2>Search for photos</h2>
-            <input type="text" onChange={this.handlerForChange}></input>
+            <input type="text" onChange={this.handlerForChange} value={this.state.searchItem}></input>
             <div className="image-list">
                 {this.state.imageUrl.map(imageObj => {
                     const {width, height}=imageObj;
